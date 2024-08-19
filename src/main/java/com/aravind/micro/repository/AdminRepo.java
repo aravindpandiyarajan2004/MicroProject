295bc56ef96adea209badbc9583dfa3507a25131
ARAVINDA RAJAN P <83591853+aravindpandiyarajan2004@users.noreply.github.com>
2024-08-19 10:05:25 +0530

    Delete src directory

--- src/main/java/com/aravind/micro/repository/AdminRepo.java
+++
@@ -1,8 +0,0 @@
-package com.aravind.micro.repository;
-
-import com.aravind.micro.model.Admin;
-
-public interface AdminRepo {
-	 Admin findByEmailAndPassword(String email, String password);
-
-}
--- src/main/java/com/aravind/micro/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.aravind.micro.service;
-
-public interface AdminService {
-	
-	String login(String email, String password);
-
-}
